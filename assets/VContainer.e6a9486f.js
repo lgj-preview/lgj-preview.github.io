9bc85e189d6a96936bbab34a6b17dd5d0946670d
Coralain <73152381+Coralain10@users.noreply.github.com>
2022-12-21 22:47:07 -0500

    cleaning duplicates

--- assets/VContainer.e6a9486f.js
+++
@@ -1,1 +0,0 @@
-import{m as t,n as o,C as s,a as r}from"./index.09b00d03.js";const l=t({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...o()},setup(e,a){let{slots:n}=a;return s(()=>r(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},n)),{}}});export{l as V};
