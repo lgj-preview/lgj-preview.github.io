67a41d0cda85197c91f7371f1c6a3216eaffa8bf
Coralain <73152381+Coralain10@users.noreply.github.com>
2022-12-21 22:47:50 -0500

    v.0.1.1
    Fixed: game links + footer logo

---
+++ assets/VContainer.957c0416.js
@@ -0,0 +1,1 @@
+import{m as t,n as o,C as s,a as r}from"./index.ec8b59ce.js";const l=t({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...o()},setup(e,a){let{slots:n}=a;return s(()=>r(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},n)),{}}});export{l as V};
