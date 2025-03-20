0328cf9f6da3779e747267c75db0de3cf9c827fd
Coralain <carolain.antoc@gmail.com>
2025-03-19 23:04:06 -0500

    Using Newsletter with own style
    + Moving to under Cover

--- assets/current.926c3b14.js
+++
@@ -1,1 +0,0 @@
-const e={overdue:"12 de enero",form:"https://forms.gle/qpueNLa1F7chU8YC9"},o={description:"30 de enero al 1 de febrero de 2026",dates:["2026-01-30","2026-01-31","2026-02-1"]},t={name:"la Escuela Toulouse Lautrec - Sede Chacarilla",url:""},r={year:2026,edition:15,registration:e,date:o,local:t};export{r as c};
